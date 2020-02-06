9dbb280688386707517ec547e09cf577a2379466
Rodrigo <rodespinoza@csumb.edu>
2020-02-06 12:52:39 -0800

    Merge branch 'master' of https://github.com/Triordan1/FinalProject5
    # Conflicts:
    #	app/src/main/java/com/example/finalproject5/Model/UserDatabase.java

--- app/src/main/java/com/example/finalproject5/Model/Course/Course.java
+++ app/src/main/java/com/example/finalproject5/Model/Course/Course.java
@@ -14,10 +14,10 @@ public class Course {
     private String instructor;
     private String title;
     private String description;
-    private Date startDate;
-    private Date endDate;
+    private String startDate;
+    private String endDate;
 
-    public Course(String instructor, String title, String description, Date startDate, Date endDate) {
+    public Course(String instructor, String title, String description, String startDate, String endDate) {
         this.instructor = instructor;
         this.title = title;
         this.description = description;
@@ -45,11 +45,11 @@ public class Course {
         return description;
     }
 
-    public Date getStartDate() {
+    public String getStartDate() {
         return startDate;
     }
 
-    public Date getEndDate() {
+    public String getEndDate() {
         return endDate;
     }
 
